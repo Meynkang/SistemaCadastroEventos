1cf6f2945fcd789c6df8f725977f7be7f14dc985
meynkang <meynkanascimento@gmail.com>
2022-01-18 12:20:08 -0300

    sistema de cadastro de eventos finalizado

---
+++ savinis.js
@@ -0,0 +1,35 @@
+// data do evento
+
+let data = 20/01/22
+
+console.log("coloque a data de seu evento")
+
+if(data >18/01/22){
+    console.log("data confirmada, prossiga")
+} else{ 
+    console.log("data inválida")
+}
+
+// idade do participante
+
+let idade = 18
+
+console.log("informe a idade")
+
+if(idade >=18){
+    console.log("participante confirmado, prossiga")
+} else{
+    console.log("idade insuficiente")
+}
+
+// lista de participantes e palestrantes e limite <=100
+
+let presentes = ("Maria", "João", "Marta", "Antônio", "André")
+
+console.log("Liste os presentes no evento", presentes.length)
+
+if(presentes.length <=100){
+    console.log("participantes e palestrantes adicionados")
+} else{
+    console.log("limite de presentes excedido")
+}
